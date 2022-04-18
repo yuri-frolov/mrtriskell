b0d2f4328ddf993160f922e7e5d61c23ca266487
Yuri Frolov <yuri.frolov.96@gmail.com>
2022-04-18 11:15:27 +0700

    test commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import mobmenu from './blocks/mobmenu';
 import menu from './blocks/menu';
+import hero from './blocks/hero';
 
 /* document.addEventListener('click', (e) => {
   console.log(e.target);
